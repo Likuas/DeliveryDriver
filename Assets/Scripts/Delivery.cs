80cb7de80107392cd978876751051ae3dbcf468d
Likuas <Demby0205@gmail.com>
2023-12-11 18:40:04 +0100

    Second commit.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] bool hasPackage = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Package" && hasPackage==false)
+        {
+            hasPackage = true;
+            this.GetComponent<SpriteRenderer>().color = Color.blue;
+            Destroy(collision.gameObject);
+        }
+        else if(collision.tag == "Customer" && hasPackage==true)
+        {
+            hasPackage = false;
+            this.GetComponent<SpriteRenderer>().color = Color.green;
+            Destroy(collision.gameObject);
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(this.gameObject.name + " kolidował z "+ collision.gameObject.name);
