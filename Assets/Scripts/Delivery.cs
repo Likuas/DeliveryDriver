4a4ca91f66e16cfff6b6f7c405f287c2267f66e5
Likuas <Demby0205@gmail.com>
2023-12-12 16:14:20 +0100

    Last Commit

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     [SerializeField] bool hasPackage = false;
-
+    SpriteRenderer spriteRenderer;
+    [SerializeField] Color32 colorHasPackage = new Color32 (1,1,1,1);
+    [SerializeField] Color32 colorDoNotHasPackage = new Color32(1,1,1,1);
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Package" && hasPackage==false)
         {
             hasPackage = true;
-            this.GetComponent<SpriteRenderer>().color = Color.blue;
-            Destroy(collision.gameObject);
+            spriteRenderer.color = colorHasPackage;
+            Destroy(collision.gameObject,0.5f);
         }
         else if(collision.tag == "Customer" && hasPackage==true)
         {
             hasPackage = false;
-            this.GetComponent<SpriteRenderer>().color = Color.green;
-            Destroy(collision.gameObject);
+            spriteRenderer.color = colorDoNotHasPackage;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField]  float moveSpeed = 10;
-    [SerializeField]  float steerSpeed = 100;
-    
+    [SerializeField]  float moveSpeed = 20;
+    [SerializeField]  float steerSpeed = 200;
+    [SerializeField] float maxSpeed = 30;
+    [SerializeField] float slowSpeed = 10;
 
     // Start is called before the first frame update
     void Start()
     {
 
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "SpeedUP")
+        {
+            moveSpeed = maxSpeed;
+        }
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Bump")
+        {
+            moveSpeed = slowSpeed;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
